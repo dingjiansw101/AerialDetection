ccf238d46f3842bf31afd32284db18cca674ae25
nijkah <nijkah@gmail.com>
2020-08-04 04:27:22 +0000

    fix import path

--- mmdet/models/necks/hrfpn.py
+++ mmdet/models/necks/hrfpn.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
-from mmcv.cnn.weight_init import caffe2_xavier_init
+from mmcv.cnn import caffe2_xavier_init
 
 from ..utils import ConvModule
 from ..registry import NECKS
